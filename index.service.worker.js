82fdc77db4fbfb2592ad43e53c8d61a0c95f046b
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2026-07-14 18:05:48 +0200

    Release 26a6

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783873668|366071289';
+const CACHE_VERSION = '1784038156|4797841430';
 /** @type {string} */
 const CACHE_PREFIX = 'Minecraft Clicke-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- test/index.service.worker.js
+++ test/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783784721|8830305474';
+const CACHE_VERSION = '1783936475|3875552583';
 /** @type {string} */
 const CACHE_PREFIX = 'Minecraft Clicke-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
